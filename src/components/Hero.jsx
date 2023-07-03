6b2e6a80884641087792b1349f57f7df0194ae5c
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-07-02 22:27:08 -0700

    Updated design.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,29 +2,31 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <section className="hero-section container d-flex justify-content-center align-items-center">
-      <div className="text-center m-5 p-5">
-        <h1 className="fs-1">
-          Hi, I'm <span className="hero-span">Raman Vandal</span>.
-        </h1>
-        <h1 className="fs-1">I'm a full stack web developer.</h1>
-        <a href='#projects' className="btn hero-btn fs-5 mt-3 p-3">
-          View my work{" "}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="var(--purple)"
-            className="bi bi-arrow-down hero-svg"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-            />
-          </svg>
-        </a>
-      </div>
-    </section>
+    <>
+      <section className="hero-section container d-flex justify-content-center align-items-center">
+        <div className="text-center m-5 p-5">
+          <h1 className="fs-1">
+            Hi, I'm <span className="hero-span">Raman Vandal</span>.
+          </h1>
+          <h1 className="fs-1">I'm a full stack web developer.</h1>
+          <a href="#projects" className="btn hero-btn fs-5 mt-3 p-3">
+            View my work{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="var(--purple)"
+              className="bi bi-arrow-down hero-svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+              />
+            </svg>
+          </a>
+        </div>
+      </section>
+    </>
   );
 };
---
+++ src/components/ParticleBackground.jsx
@@ -0,0 +1,10 @@
+import Particles from 'react-tsparticles';
+import particleConfig from './config/particle-config';
+
+const ParticleBackground = () => {
+  return (
+    <Particles params={particleConfig}></Particles>
+  )
+}
+
+export default ParticleBackground
