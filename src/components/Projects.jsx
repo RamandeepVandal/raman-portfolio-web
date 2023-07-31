4be8cca61f50301c402fbf5f485082bed127ab18
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-07-31 15:53:02 -0700

    Changed the links and styles of the proj demos.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 // project images
-import dramaList from "../assets/DramaList.mp4";
-import otterClone from "../assets/Otter-Clone (1) (1).mp4";
-import restCountries from "../assets/rest-countries_compressed.mp4";
-import shoeFlex from "../assets/ShoeFlex (1).mp4";
+import otterClone from "../assets/otter-final.gif";
+import restCountries from "../assets/rest-country-final.gif";
+import shoeFlex from "../assets/shoeflex-final.gif";
+import dramaGif from "../assets/dramalist-final.gif";
 
 export const Projects = () => {
   const { ref: myRef, inView: sectionIsVisible } = useInView();
@@ -29,59 +29,24 @@ export const Projects = () => {
             <div className="col-12 col-md-12 col-lg-6">
               <div
                 ref={myRef}
-                className={`card d-flex m-5 p-5 justify-content-center align-items-center text-center proj-card`}
+                className={`card m-5 d-flex justify-content-center align-items-center text-center proj-card`}
               >
-                <div className="embed-responsive embed-responsive-16by9">
-                  <video
-                    className="img-fluid"
-                    src={restCountries}
-                    controls
-                    muted
-                  ></video>
-                </div>
-                <div className="mt-5">
-                  <h1 className="fs-5">REST Countries</h1>
-                  <p>
-                    Uses the REST Countries API to allow users to search for
-                    countries, view details about each country, and filter
-                    countries by continent.
+                <img
+                  src={dramaGif}
+                  alt="dramalist demo"
+                  className="img-fluid card-img-top"
+                />
+                <div className="p-5">
+                  <h1 className="fs-3">DramaList</h1>
+                  <p className="fs-5">
+                    Allows users to keep track of the dramas they
+                    have watched and search for new dramas using the TMDB API.
                   </p>
                   <p>
-                    <span style={{fontWeight: 'bold'}} className="hero-span">Tech Stack:</span> React, Bootstrap, REST APIs
-                  </p>
-                  <div className="d-flex justify-content-evenly align-items-center text-center card-btn">
-                    <a
-                      href="https://github.com/RamandeepVandal/rest-api-countries"
-                      className="btn hero-btn"
-                    >
-                      Code
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-12 col-lg-6">
-              <div
-                ref={myRef}
-                className={`card d-flex m-5 p-5 justify-content-center align-items-center text-center proj-card`}
-              >
-                <div className="embed-responsive embed-responsive-16by9">
-                  <video
-                    className="img-fluid"
-                    src={dramaList}
-                    controls
-                    muted
-                  ></video>
-                </div>
-                <div className="mt-5">
-                  <h1 className="fs-5">DramaList</h1>
-                  <p>
-                    Allows users to manage and keep track of the dramas they
-                    have watched. The project is built using React, Bootstrap,
-                    Node.js, MongoDB.
-                  </p>
-                  <p>
-                    <span style={{fontWeight: 'bold'}} className="hero-span">Tech Stack:</span> React, MongoDB, ExpressJS
+                    <span style={{ fontWeight: "bold" }} className="hero-span">
+                      Tech Stack:
+                    </span>{" "}
+                    React, MongoDB, ExpressJS, NodeJS
                   </p>
                   <div className="d-flex justify-content-evenly align-items-center text-center card-btn">
                     <a
@@ -97,22 +62,54 @@ export const Projects = () => {
             <div className="col-12 col-md-12 col-lg-6">
               <div
                 ref={myRef}
-                className={`card d-flex m-5 p-5 justify-content-center align-items-center text-center proj-card`}
+                className={`card d-flex m-5 justify-content-center align-items-center text-center proj-card`}
               >
-                <video
-                  className="img-fluid"
-                  src={otterClone}
-                  controls
-                  muted
-                ></video>
-                <div className="mt-5">
-                  <h1 className="fs-5">Otter Landing Page Clone</h1>
+                <img
+                  src={shoeFlex}
+                  alt="dramalist demo"
+                  className="img-fluid card-img-top"
+                />
+                <div className="p-5">
+                  <h1 className="fs-3">ShoeFlex Store</h1>
+                  <p className="fs-5">
+                    Fully responsive MERN app that allows users to view the
+                    products and place orders with a working cart system.
+                  </p>
                   <p>
+                    <span style={{ fontWeight: "bold" }} className="hero-span">
+                      Tech Stack:
+                    </span>{" "}
+                    React, Bootstrap, MongoDB, NodeJS
+                  </p>
+                  <div className="d-flex justify-content-evenly align-items-center text-center card-btn">
+                    <a href="#" className="btn hero-btn">
+                      Code
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-12 col-lg-6">
+              <div
+                ref={myRef}
+                className={`card d-flex m-5 justify-content-center align-items-center text-center proj-card`}
+              >
+                <img
+                  src={otterClone}
+                  alt="dramalist demo"
+                  className="img-fluid card-img-top"
+                />
+                <div className="p-5">
+                  <h1 className="fs-3">Otter Landing Page Clone</h1>
+                  <p className="fs-5">
                     This front-end web app uses React and Bootstrap to create a
                     clone of the Otter landing page. It is fully responsive!
                   </p>
                   <p>
-                    <span style={{fontWeight: 'bold'}} className="hero-span">Tech Stack:</span> React, Bootstrap
+                    <span style={{ fontWeight: "bold" }} className="hero-span">
+                      Tech Stack:
+                    </span>{" "}
+                    React, Bootstrap
                   </p>
                   <div className="d-flex justify-content-evenly align-items-center text-center card-btn">
                     <a
@@ -128,25 +125,30 @@ export const Projects = () => {
             <div className="col-12 col-md-12 col-lg-6">
               <div
                 ref={myRef}
-                className={`card d-flex m-5 p-5 justify-content-center align-items-center text-center proj-card`}
+                className={`card d-flex m-5 justify-content-center align-items-center text-center proj-card`}
               >
-                <video
-                  className="img-fluid"
-                  src={shoeFlex}
-                  controls
-                  muted
-                ></video>
-                <div className="mt-5">
-                  <h1 className="fs-5">ShoeFlex Store</h1>
-                  <p>
-                    Fully responsive MERN app that allows users to view the
-                    products and place orders with a working cart system.
+                <img
+                  src={restCountries}
+                  alt="dramalist demo"
+                  className="img-fluid card-img-top"
+                />
+                <div className="p-5">
+                  <h1 className="fs-3">REST Countries</h1>
+                  <p className="fs-5">
+                    Uses the REST Countries API to allow users to search for
+                    countries, and view details about each country.
                   </p>
                   <p>
-                    <span style={{fontWeight: 'bold'}} className="hero-span">Tech Stack:</span> React, Bootstrap, MongoDB, Express
+                    <span style={{ fontWeight: "bold" }} className="hero-span">
+                      Tech Stack:
+                    </span>{" "}
+                    React, Bootstrap, REST APIs
                   </p>
                   <div className="d-flex justify-content-evenly align-items-center text-center card-btn">
-                    <a href="#" className="btn hero-btn">
+                    <a
+                      href="https://github.com/RamandeepVandal/rest-api-countries"
+                      className="btn hero-btn"
+                    >
                       Code
                     </a>
                   </div>
