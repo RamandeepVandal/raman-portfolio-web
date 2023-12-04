59d254b5e79d27c3d89d24303bfb167554e1dd63
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-12-03 19:33:38 -0800

    Initial commit.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,64 @@
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import { useInView } from "react-intersection-observer";
+
+export const Contact = () => {
+  const { ref: myRef, inView: sectionIsVisible } = useInView({
+    triggerOnce: true,
+  });
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, import.meta.env.VITE_USER_ID)
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <section className="mt-5">
+      <div
+        className={`d-flex flex-column justify-content-center align-items-center`}
+        id="contact"
+      >
+        <h2 ref={myRef} className={`hidden ${sectionIsVisible ? "show" : ""}`}>
+          Contact
+        </h2>
+        <hr />
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+              <form
+                className={`card about-card m-5 p-5 hidden ${
+                  sectionIsVisible ? "show" : ""
+                }`}
+                ref={form}
+                onSubmit={sendEmail}
+              >
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control form-control-contact" id="name" name="user_name" />
+                  <label htmlFor="name" className="form-label">Name</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input type="email" className="form-control form-control-contact" id="email" name="user_email"/>
+                  <label htmlFor="email" className="form-label">Email</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <textarea type="text" className="form-control form-control-contact" id="message" name="message"/>
+                  <label htmlFor="message" className="form-label">Message</label>
+                </div>
+                <button className="btn hero-btn fs-5 mt-3 p-3" type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
