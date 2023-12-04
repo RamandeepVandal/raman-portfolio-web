b225572ab4d3633abbc2b94247aa38d40c849711
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-12-03 23:36:46 -0800

    Removed spinner.

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -3,48 +3,20 @@ import { Hero } from "../components/Hero";
 import { About } from "../components/About";
 import { Skills } from "../components/Skills";
 import { Footer } from "../components/Footer";
-// loading spinner
-import { Spinner } from "react-bootstrap";
 import { Header } from "../components/Header";
 import { Contact } from "../components/Contact";
 
 export const HomePage = () => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-  }, []);
-
   return (
     <div>
-      {loading ? (
-        <div>
-          <Header />
-          <div className="App">
-            <Spinner
-              animation="border"
-              role="status"
-              size="lg"
-              className="spinner"
-            >
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <Header />
-          <Hero />
-          <About />
-          <Skills />
-          <Contact />
-          <Footer />
-        </div>
-      )}
+      <div>
+        <Header />
+        <Hero />
+        <About />
+        <Skills />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 };
