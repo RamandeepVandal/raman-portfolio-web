71e48d025613f2c8f11a0b2e8c0906bc8fca8c68
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-12-03 19:34:50 -0800

    Added the spinner component.

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import { Hero } from '../components/Hero';
-import { About } from '../components/About';
-import { Skills } from '../components/Skills';
-import { Footer } from '../components/Footer';
+import React, { useEffect, useState } from "react";
+import { Hero } from "../components/Hero";
+import { About } from "../components/About";
+import { Skills } from "../components/Skills";
+import { Footer } from "../components/Footer";
+// loading spinner
+import { Spinner } from "react-bootstrap";
+import { Header } from "../components/Header";
+import { Contact } from "../components/Contact";
 
 export const HomePage = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   return (
     <div>
-        <Hero />
-        <About />
-        <Skills />
-        <Footer />
+      {loading ? (
+        <div>
+          <Header />
+          <div className="App">
+            <Spinner
+              animation="border"
+              role="status"
+              size="lg"
+              className="spinner"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Hero />
+          <About />
+          <Skills />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
