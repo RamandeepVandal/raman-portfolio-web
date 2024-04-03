a01cfb47f597caec8418433ca5df4f0e0b93aa59
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2024-04-02 17:07:39 -0700

    Updated the particles component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // screen
 import { HomePage } from "./screens/HomePage"
+import ParticlesComp from "./components/ParticlesComp";
 
 function App() {
   return (
+    <>
+    <ParticlesComp id="particles" />
     <Router basename="/raman-portfolio-web">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
       </Routes>
     </Router>
+    </>
   )
 }
 
